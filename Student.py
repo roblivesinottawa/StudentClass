2ee059a2419e368a99a9c66922a9f0b85d333494
roblivesinottawa <tech.rob@icloud.com>
2021-01-19 12:55:06 -0500

    first commmit

---
+++ Student.py
@@ -0,0 +1,34 @@
+class Student:
+
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def get_grade(self):
+        return self.grade
+
+
+class Course:
+
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+
+    def get_average_grade(self):
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+        return value / len(self.students)
+
+
+student_one = Student("Rob", 34, 89)
+student_two = Student("Vick", 34, 97)
+print(student_one)
